2dd8eb68695279bad7cd8705c1bda3639954d0e7
spaceC00kie <kirstie317@gmail.com>
2025-07-05 22:45:46 -0400

    refactor: update project configuration and dependencies for sky-see-video

--- firestore.config.ts
+++ firestore.config.ts
@@ -1,11 +1,11 @@
 import { initializeApp } from "firebase/app"
 
 export const firebaseApp = initializeApp({
-  apiKey: "AIzaSyDmBaAXrP8FGKbchlTHTZzKCOZqhZSH6AY",
-  authDomain: "has-the-dog-been-fed-f9102.firebaseapp.com",
-  projectId: "has-the-dog-been-fed-f9102",
-  storageBucket: "has-the-dog-been-fed-f9102.appspot.com",
-  messagingSenderId: "46976099487",
-  appId: "1:46976099487:web:8e17ada9ee2cf899bfaeaa",
-  measurementId: "G-QNVCXH0G3Q"
+  apiKey: "AIzaSyD5ONO-SgJ4aikM2stpkq-S890HP6zC-AM",
+  authDomain: "sky-see-video.firebaseapp.com",
+  projectId: "sky-see-video",
+  storageBucket: "sky-see-video.firebasestorage.app",
+  messagingSenderId: "353518650806",
+  appId: "1:353518650806:web:94d42ebae8322709d12d17",
+  measurementId: "G-8KMS1CZ8CF",
 })
--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,17 @@ import { motion } from "framer-motion"
 import { Header } from "./components/header/Header"
 import { MainContent } from "./components/body/MainContent"
 import { Auth } from "./containers/Auth"
+import ReactGA from "react-ga4"
 
 export const App: React.FC = () => {
   const { auth } = Auth.useContainer()
   const overflow = auth?.currentUser ? "overflow-clip" : "overflow-y-scroll"
+  ReactGA.initialize("G-8KMS1CZ8CF", {
+    gtagOptions: {
+      cookie_flags: "SameSite=None;Secure",
+    },
+  })
+
   return (
     <motion.div
       id="app"
