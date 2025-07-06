f3d5a925105b279e658908645ffa9eb40f504c52
spaceC00kie <kirstie317@gmail.com>
2025-07-06 00:36:47 -0400

    refactor: improve layout and styling for navigation links and contact information in HeaderRight component

--- src/components/header/HeaderRight.tsx
+++ src/components/header/HeaderRight.tsx
@@ -1,25 +1,40 @@
 import { NavLink } from "react-router-dom"
 
 export const HeaderRight: React.FC = () => (
-  <div className="mx-1 flex flex-auto flex-nowrap items-center justify-end gap-6 px-2 align-middle text-xs">
-    <NavLink to="/" className="whitespace-nowrap hover:text-blue-400">
+  <div className="mx-1 flex flex-auto flex-nowrap items-end justify-end gap-6 px-2 text-xs">
+    <NavLink to="/" className="whitespace-nowrap pb-2 hover:text-blue-400">
       HOME
     </NavLink>
-    <NavLink to="/about-us" className="whitespace-nowrap hover:text-blue-400">
+    <NavLink
+      to="/about-us"
+      className="whitespace-nowrap pb-2 hover:text-blue-400"
+    >
       ABOUT US
     </NavLink>
-    <NavLink to="/our-work" className="whitespace-nowrap hover:text-blue-400">
+    <NavLink
+      to="/our-work"
+      className="whitespace-nowrap pb-2 hover:text-blue-400"
+    >
       OUR WORK
     </NavLink>
-    <NavLink to="/our-services" className="whitespace-nowrap hover:text-blue-400">
+    <NavLink
+      to="/our-services"
+      className="whitespace-nowrap pb-2 hover:text-blue-400"
+    >
       OUR SERVICES
     </NavLink>
-    <NavLink to="/contact-us" className="whitespace-nowrap hover:text-blue-400">
+    <NavLink
+      to="/contact-us"
+      className="whitespace-nowrap pb-2 hover:text-blue-400"
+    >
       CONTACT US
     </NavLink>
-    <div className="flex flex-col gap-1 whitespace-nowrap">
-      <div>678-304-9920</div>
-      <NavLink to="/contact-us" className="whitespace-nowrap rounded-md bg-blue-500 p-2 text-white hover:bg-blue-300">
+    <div className="flex flex-col gap-2 whitespace-nowrap">
+      <div className="pt-4 text-center">678-304-9920</div>
+      <NavLink
+        to="/contact-us"
+        className="whitespace-nowrap rounded-md bg-blue-500 px-3 py-2 text-white hover:bg-blue-300"
+      >
         GET QUOTE
       </NavLink>
     </div>
