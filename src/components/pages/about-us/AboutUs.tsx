62f32c1111c6d19332d95ffc88f402d4ce261a93
spaceC00kie <kirstie317@gmail.com>
2025-07-06 17:44:36 -0400

    Add TedsAboutUsCard component for About Us page
    - Introduced TedsAboutUsCard component to showcase Ted Intorcio, Creative Director.
    - Included an image, name, title, and a brief description of his expertise and experience.
    - Added a link to Ted's personal portfolio for further exploration of his work.

--- src/components/pages/about-us/AboutUs.tsx
+++ src/components/pages/about-us/AboutUs.tsx
@@ -1,4 +1,8 @@
 import { AboutUsCard } from "./AboutUsCard"
+import { default as sorenChristiansen } from "/src/assets/about-us/soren-christiansen.jpg"
+import { default as robinsonVil } from "/src/assets/about-us/robinson-vil.jpg"
+import { default as charlesFrederick } from "/src/assets/about-us/charles-frederick.jpg"
+import { TedsAboutUsCard } from "./TedsAboutUsCard"
 
 interface Props {}
 
@@ -20,11 +24,25 @@ export const AboutUs: React.FC<Props> = ({}) => {
         storytelling.
       </p>
       <div className="grid gap-5 md:grid-cols-2">
-        {/* fill in details here */}
-        <AboutUsCard image={""} title="" description="" />
-        <AboutUsCard image={""} title="" description="" />
-        <AboutUsCard image={""} title="" description="" />
-        <AboutUsCard image={""} title="" description="" />
+        <AboutUsCard
+          image={sorenChristiansen}
+          name="Soren Christiansen"
+          title="Videographer / Editor / Drone Pilot"
+          description="With over 3 decades in advertising and video production, Soren uses his knowledge and experience to make sure every video package hits its mark and achieves its targeted goals. Soren's work experience and clients include McCann-Erickson, CNN, CNNi, Kia, Hyundai HCEA, Esso/Exxon, Mastercard, Motorola, Pollo Tropical, This American Land on PBS, and more. Soren is a videographer, licensed drone pilot and video editor."
+        />
+        <TedsAboutUsCard />
+        <AboutUsCard
+          image={robinsonVil}
+          name="Robinson Vil"
+          title="Director / Producer"
+          description="Rob, a native of Haiti, is an award-winning director & producer who specializes in films and documentaries. His nominations in the film category include the Pan African Film Festival, the Bare Bones Intl Film Festival, the Cannes Short Film Corners, and winning Best Actor, Best Director and Best Picture at the Motion Picture Association of Haiti for “Rasin Mwen—L’ amour Du Fric.”"
+        />
+        <AboutUsCard
+          image={charlesFrederick}
+          name="Charles Frederick"
+          title="Producer / Social Media"
+          description="Charles is a multi-Emmy award winning writer, producer and editor, creating content for television, radio, online and digital communications. Specializing in producing content for social media, Charles ensures our messaging hits the mark and targets the right people for your products and services. Charles is a broadcast veteran of CNN, CBS Los Angeles, Fox Sports, Bally Sports and NBC San Diego."
+        />
       </div>
     </div>
   )
---
+++ src/components/pages/about-us/TedsAboutUsCard.tsx
@@ -0,0 +1,34 @@
+import { default as tedIntorcio } from "/src/assets/about-us/ted-intorcio.jpg"
+
+export const TedsAboutUsCard: React.FC = ({}) => {
+  return (
+    <div className="flex flex-col items-start gap-2 text-lg">
+      <div className="aspect-[4/3] w-full">
+        <img
+          src={tedIntorcio}
+          alt={"Ted Intorcio"}
+          className="h-full w-full object-cover"
+        />
+      </div>
+      <span className="pointer-events-none absolute inset-0 bg-white opacity-0 transition-opacity group-hover:opacity-40" />
+      <div className="text-blue-500">
+        <h2>{"Ted Intorcio"}</h2>
+        <h2>{"Creative Director"}</h2>
+      </div>
+      <p className="text-sm">
+        {
+          "Ted loves stuff that moves. Combined with his passion for the magic of visual communication design, typography, illustration and branding, he has created a unique set of skills that have successfully been employed for clients like Turner Broadcasting, Starz Entertainment, ESPN, NatGeo and Awkward Family Photos. Ted knows design and how to make the most of the limited time and space each video has to make your company shine. You can see his personal website "
+        }
+        <a
+          href="https://tintorcio.myportfolio.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-blue-600 underline hover:text-blue-800"
+        >
+          here
+        </a>
+        {" to explore the breadth of his talents."}
+      </p>
+    </div>
+  )
+}
