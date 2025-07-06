16b07ab4687419b328d704baba8feb9947ee4e00
geektechniquestudios <geektechniquestudios@gmail.com>
2025-07-06 00:28:55 -0400

    refactor: update Header component layout and enhance HeaderLeft with routing

--- src/components/header/HeaderLeft.tsx
+++ src/components/header/HeaderLeft.tsx
@@ -1,9 +1,8 @@
 import { default as skyseeLogo } from "/src/assets/skysee-7.png"
+import { Link } from "react-router-dom"
 
-export const HeaderLeft: React.FC = () => {
-  return (
-    <div className="flex-auto">
-      <img src={skyseeLogo} />
-    </div>
-  )
-}
+export const HeaderLeft: React.FC = () => (
+  <Link to="/" className="flex-none py-1">
+    <img src={skyseeLogo} className="h-16 w-auto" />
+  </Link>
+)
--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,5 +1,18 @@
+import ReactPlayer from "react-player"
+
 interface Props {}
 
 export const Home: React.FC<Props> = ({}) => {
-  return <div>Hello from home </div>
+  return (
+    
+      <ReactPlayer
+        src="https://vimeo.com/806447198"
+        playing
+        muted
+        loop
+        width="100%"
+        height="100%"
+      />
+    
+  )
 }
