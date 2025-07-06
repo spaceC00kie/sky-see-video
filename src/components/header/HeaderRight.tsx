97e9ef31f0eb85c933d443bde7a8b4efa536e8c5
spaceC00kie <kirstie317@gmail.com>
2025-07-05 23:48:47 -0400

    refactor: enhance styling and spacing for navigation links in HeaderRight component

--- src/components/header/HeaderRight.tsx
+++ src/components/header/HeaderRight.tsx
@@ -1,11 +1,25 @@
-import { NavLink } from 'react-router-dom';
+import { NavLink } from "react-router-dom"
 
 export const HeaderRight: React.FC = () => (
-  <div className="mx-1 flex flex-auto items-center justify-end gap-1.5 px-2 align-middle">
-    <NavLink to="/">HOME</NavLink>
-    <NavLink to="/about">ABOUT US</NavLink>
-    <NavLink to="/work">OUR WORK</NavLink>
-    <NavLink to="/services">OUR SERVICES</NavLink>
-    <NavLink to="/contact">CONTACT US</NavLink>
+  <div className="mx-1 flex flex-auto items-center justify-end gap-6 px-2 align-middle text-xs">
+    <NavLink to="/" className="hover:text-blue-400">
+      HOME
+    </NavLink>
+    <NavLink to="/about" className="hover:text-blue-400">
+      ABOUT US
+    </NavLink>
+    <NavLink to="/work" className="hover:text-blue-400">
+      OUR WORK
+    </NavLink>
+    <NavLink to="/services" className="hover:text-blue-400">
+      OUR SERVICES
+    </NavLink>
+    <NavLink to="/contact" className="hover:text-blue-400">
+      CONTACT US
+    </NavLink>
+    <div>
+      <div>678-304-9920</div>
+      <NavLink to="/contact">GET QUOTE</NavLink>
+    </div>
   </div>
-);
+)
