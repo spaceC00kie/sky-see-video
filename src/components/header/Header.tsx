c579d3471ffedbb379aad321091e0a049039f246
geektechniquestudios <geektechniquestudios@gmail.com>
2025-07-06 18:45:41 -0400

    fix: add padding to Header component for improved layout

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -4,7 +4,7 @@ import { HeaderRight } from "./HeaderRight"
 
 export const Header: React.FC = () => {
   return (
-    <div className="sticky top-0 z-50 flex h-20 w-full shrink-0 items-center justify-center bg-white px-4 text-xl">
+    <div className="sticky top-0 z-50 flex h-20 w-full shrink-0 items-center justify-center bg-white px-4 text-xl pb-3">
       <div className="no-wrap flex w-full justify-between relative">
         <HeaderLeft />
         <HeaderMiddle />
