b7c99d38b8b69f746c227e5a8c55ffacb10aadbb
geektechniquestudios <geektechniquestudios@gmail.com>
2025-07-05 23:25:03 -0400

    Refactor code structure for improved readability and maintainability

--- src/components/header/HeaderLeft.tsx
+++ src/components/header/HeaderLeft.tsx
@@ -1,3 +1,9 @@
+import { default as skyseeLogo } from "/src/assets/skysee-7.png"
+
 export const HeaderLeft: React.FC = () => {
-  return <div className="flex-auto"></div>
+  return (
+    <div className="flex-auto">
+      <img src={skyseeLogo} />
+    </div>
+  )
 }
--- src/components/header/HeaderRight.tsx
+++ src/components/header/HeaderRight.tsx
@@ -4,8 +4,11 @@ import { SignOutButton } from "./SignOutButton"
 export const HeaderRight: React.FC = () => {
   return (
     <div className="mx-1 flex flex-auto items-center justify-end gap-1.5 px-2 align-middle">
-      <SignInButton />
-      <SignOutButton />
+      <button>HOME</button>
+      <button>ABOUT US</button>
+      <button>OUR WORK</button>
+      <button>OUR SERVICES</button>
+      <button>CONTACT US</button>
     </div>
   )
 }
