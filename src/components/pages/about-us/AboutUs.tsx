34d0b4d18ae5ab33a19594f9dc2f0210927146f1
geektechniquestudios <geektechniquestudios@gmail.com>
2025-07-07 12:58:35 -0400

    feat: adjust padding in About Us component for improved layout

--- src/components/pages/about-us/AboutUs.tsx
+++ src/components/pages/about-us/AboutUs.tsx
@@ -11,7 +11,7 @@ export const AboutUs: React.FC<Props> = ({}) => {
   return (
     <div className="flex flex-col items-center justify-center">
       <img src={aboutUsBanner} className="sm:h-96 h-32 w-full object-cover object-top" />
-      <div className="flex max-w-[70em] flex-col gap-4 px-6 py-5">
+      <div className="flex max-w-[70em] flex-col gap-4 px-6 pt-5 pb-40">
         <p className="text-2xl text-blue-500">ABOUT US</p>
         <p className="p-2 text-xl">
           You’ve got specific media needs, and SkySee Video has the expertise to
