4369a1a1214733b9231c07ea2253d735e75e0ba8
spaceC00kie <60335398+spaceC00kie@users.noreply.github.com>
2025-07-09 04:45:35 -0400

    fix: resolve Tailwind build errors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     listStyleType: {
       square: "square",
     },
-    extend: {
-      corePlugins: {
-        contentVisibility: true,
-      },
-    },
+    extend: {},
+  },
+  corePlugins: {
+    contentVisibility: true,
   },
   plugins: [],
 }
