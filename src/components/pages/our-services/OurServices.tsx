bc554272eef2015ee605f168af25a509df5b0238
Geek Technique <42378648+geektechniquestudios@users.noreply.github.com>
2025-07-09 17:18:25 -0400

    fix: show full descriptions on services page

--- src/components/pages/our-services/OurServices.tsx
+++ src/components/pages/our-services/OurServices.tsx
@@ -33,7 +33,10 @@ export const OurServices: React.FC<Props> = () => (
 
         <p className="text-2xl">
           SkySee Video offers pre-production, production and post-production
-          services and packaging.&nbsp;…
+          services and packaging. We work on corporate marketing videos,
+          training videos, social media events, promotional videos, and more.
+          Our experience both in the air and on the ground makes us the right
+          team for short and long form documentaries. These services include:
         </p>
 
         <div className="flex flex-col flex-wrap justify-evenly gap-5 sm:flex-row">
@@ -80,25 +83,25 @@ export const OurServices: React.FC<Props> = () => (
           <OurServicesCard2
             image={construction}
             title="Construction"
-            description="Stand out at conventions, trade shows, social media and more. …"
+            description="Stand out at conventions, trade shows, social media and more. SkySee Video helps today’s home builders and developers highlight the uniqueness of their properties and developments with broadcast quality video packaging. Builders who are LEED compliant can take advantage of SkySee Video’s infrared cameras, emphasizing your company’s green commitment to the environment."
             path=""
           />
           <OurServicesCard2
             image={tourism}
             title="Tourism & Resort"
-            description="Inspire travelers to pack their bags and book your rooms by capturing the beauty and uniqueness of your vacation paradise. …"
+            description="Inspire travelers to pack their bags and book your rooms by capturing the beauty and uniqueness of your vacation paradise. Promote the once-in-a-lifetime experience you have to offer with broadcast quality video, shot on the ground and in the air. SkySee Video works with marketing teams to provide promotion video packages for social media, trade shows, conventions and more."
             path=""
           />
           <OurServicesCard2
             image={corporate}
             title="Corporate Marketing"
-            description="Capture your target audience with professional, polished video packaging, including 2-D and 3-D animation. …"
+            description="Capture your target audience with professional, polished video packaging, including 2-D and 3-D animation. Whether you’re a B2B, B2C or not-for-profit organization, SkySee Video has the solution. SkySee Video works with marketing teams to provide broadcast quality video, shot on the ground, and in the air for social media, trade shows, conventions and more."
             path=""
           />
           <OurServicesCard2
             image={documentaries}
             title="Documentaries"
-            description="Broadcast quality on-ground and aerial cinematography, field producing, DAM, 2-D & 3-D animation and more. …"
+            description="Broadcast quality, on the ground and aerial cinematography, field producing, DAM, 2-D & 3-D animation, and more. SkySee Video has everything you need for your next documentary, including gimbals for smooth run ‘n gun interviews, walk ‘n talks, and b-roll. SkySee Video uses Frame.io’s Camera to Cloud, allowing clients/producers to view shots in real time."
             path=""
           />
         </div>
