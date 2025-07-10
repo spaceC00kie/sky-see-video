14a22a51f579f2a528bd51a41cb16a1fe52006d3
geektechniquestudios <geektechniquestudios@gmail.com>
2025-07-10 18:21:01 -0400

    update margins for videos

--- src/components/pages/services/Construction.tsx
+++ src/components/pages/services/Construction.tsx
@@ -5,7 +5,7 @@ interface Props {}
 export const Construction: React.FC<Props> = ({}) => {
   return (
     <div className="flex flex-col items-center justify-center">
-      <div className="relative mx-auto my-6 aspect-video w-full max-w-[95em]">
+      <div className="relative mx-auto mb-6 aspect-video w-full max-w-[95em]">
         <div className="absolute inset-0 grid place-content-center rounded-sm bg-black" />
         <iframe
           src="https://player.vimeo.com/video/193616058?h=af982b45c1&badge=0&autopause=0&player_id=0&app_id=58479"
