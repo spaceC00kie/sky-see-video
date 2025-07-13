1e60ed253653caf10b9e5fc607486af5b878e273
geektechniquestudios <geektechniquestudios@gmail.com>
2025-07-12 20:45:21 -0400

    update z index of text in hero banner

--- src/components/common/HeroBanner.tsx
+++ src/components/common/HeroBanner.tsx
@@ -28,7 +28,7 @@ export const HeroBanner: React.FC<Props> = ({
       <div className="absolute inset-0 flex items-end justify-center pb-20">
         <div className="relative inline-block">
           <span className="pointer-events-none absolute inset-1 rounded-full bg-stone-900/80 blur-2xl" />
-          <p className="relative z-10 px-6 py-4 text-center text-3xl text-white drop-shadow-2xl">
+          <p className="relative z-50 px-6 py-4 text-center text-3xl text-white drop-shadow-2xl">
             {overlayText}
           </p>
         </div>
