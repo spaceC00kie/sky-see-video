f4425a8fa610bd52bad9c5dcef2efbc70380d2ad
geektechniquestudios <geektechniquestudios@gmail.com>
2025-07-22 00:28:29 -0400

    chore: remove sanity check placeholder from HeaderMiddle component

--- src/components/header/HeaderMiddle.tsx
+++ src/components/header/HeaderMiddle.tsx
@@ -15,7 +15,6 @@ export const HeaderMiddle: React.FC = () => {
           <img src={skyseeLogo} alt="Sky See Video" className="h-12 w-auto" />
         </Link>
       )}
-      <div>sanity check</div>
     </div>
   )
 }
