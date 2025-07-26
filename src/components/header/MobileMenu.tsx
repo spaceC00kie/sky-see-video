8b3a1c5ad82c11840852fc90fc9de479dd70e39f
geektechniquestudios <geektechniquestudios@gmail.com>
2025-07-25 20:35:58 -0400

    lint test

--- src/components/header/MobileMenu.tsx
+++ src/components/header/MobileMenu.tsx
@@ -28,7 +28,9 @@ export const MobileMenu: React.FC<Props> = ({ open, onClose }) => (
         open ? "translate-x-0" : "-translate-x-full"
       }`}
     >
-      <div className="flex flex-col">
+      <div
+        className="flex flex-col"
+      >
         <div className="flex w-full justify-end">
           <button onClick={onClose} className="p-2" aria-label="Close menu">
             <IoClose size={35} />
